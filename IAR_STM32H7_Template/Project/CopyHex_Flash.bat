5f73b3db8ebc93b7172dc1c4b5a39f83681e9770
Dell_Raynen <1610564369@qq.com>
2022-07-21 17:27:39 +0800

    IAR_STM32H7_Template

---
+++ IAR_STM32H7_Template/Project/CopyHex_Flash.bat
@@ -0,0 +1,2 @@
+REM %~sdp0 表示bat文件的目录(短名字)  %~dp0 表示bat文件的目录(长文件名)
+copy %~sdp0Debug\Exe\IAR_Template.hex %~sdp0..\output(iar).hex
---
+++ IAR_STM32H7_Template/删除目标文件(用于打包备份).bat
@@ -0,0 +1,8 @@
+del /Q project\Project.dep
+del /Q project\Debug
+del /Q project\Flash
+del /Q project\settings
+del /Q project\Debug
+rd  /Q /S project\Flash
+rd /Q /S project\settings
+rd /Q /S project\Debug
