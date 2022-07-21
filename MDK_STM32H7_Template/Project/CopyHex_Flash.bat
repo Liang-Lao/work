7a721adcd686e1913b792a4d46d38815fe63b0d0
Dell_Raynen <1610564369@qq.com>
2022-07-21 17:24:06 +0800

    MDK_STM32H7_Template

---
+++ MDK_STM32H7_Template/Project/CopyHex_Flash.bat
@@ -0,0 +1,2 @@
+REM %~sdp0 表示bat文件的目录(短名字)  %~dp0 表示bat文件的目录(长文件名)
+copy %~sdp0Objects\MDK_Template.hex %~sdp0..\output(MDK).hex
---
+++ MDK_STM32H7_Template/删除目标文件(用于打包备份).bat
@@ -0,0 +1,7 @@
+rd /Q /S project\Objects
+rd /Q /S project\Listings
+rd /Q /S project\DebugConfig
+del /Q project\*.bak
+del /Q project\*.dep
+del /Q project\JLink*
+del /Q project\project.uvgui.*
